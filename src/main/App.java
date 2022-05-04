6fa5b3f4de2c537c854f74ac0aa9d47bbb1b0601
Quezia <queziaamenezes@gmail.com>
2022-05-04 17:11:59 -0300

    Send message no app

--- src/main/App.java
+++ src/main/App.java
@@ -1,10 +1,12 @@
 package main;
 
+import br.com.sns_aws.services.SNSSendMessage;
+
 public class App 
 {
     public static void main( String[] args )
     {
-     
+    	SNSSendMessage.sendMessage("Uma mensagem SNS");
     }
 }
 
