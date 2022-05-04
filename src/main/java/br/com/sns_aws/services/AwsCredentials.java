40c80df157b48b3b8fb780299b47b973bb8b96e9
Quezia <queziaamenezes@gmail.com>
2022-05-04 16:21:15 -0300

    Import credentials

--- src/main/java/br/com/sns_aws/services/AwsCredentials.java
+++ src/main/java/br/com/sns_aws/services/AwsCredentials.java
@@ -1,5 +1,8 @@
 package main.java.br.com.sns_aws.services;
 
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider; 
+
 public class AwsCredentials {
   public static AwsCredentialsProvider returnCredentials() {
       AwsCredentialsProvider credentialsProvider = new AwsCredentialsProvider() {
